7771c184db6c602c2acb4e58b9d8ce8a3a9f1bb4
77f315 <77f315@gmail.com>
2025-03-31 01:23:33 +0300

    splash screen

---
+++ naidisprojektmobile/naidisprojektmobile/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using naidisprojektmobile.Views;
+namespace naidisprojektmobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SplashPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ naidisprojektmobile/naidisprojektmobile/Views/SplashPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Xamarin.Forms;
+
+namespace naidisprojektmobile.Views
+{
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+
+            SplashImage.Source = ImageSource.FromResource("naidisprojektmobile.Images.splash.png",
+                typeof(SplashPage).GetTypeInfo().Assembly);
+        }
+    }
+}
