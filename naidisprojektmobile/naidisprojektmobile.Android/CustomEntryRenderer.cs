b3ec0f3ce986f7a8930489060f66354d1204c598
77f315 <77f315@gmail.com>
2025-03-31 11:36:18 +0300

    remove the damn underline from entry field for android devices

---
+++ naidisprojektmobile/naidisprojektmobile.Android/CustomEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using naidisprojektmobile.Droid; // Change this to your own project's namespace
+using Android.Widget;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace naidisprojektmobile.Droid // Change this to your own project's namespace
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
