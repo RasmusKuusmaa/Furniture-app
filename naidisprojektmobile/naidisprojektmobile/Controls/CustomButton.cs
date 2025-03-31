f46e5f791f4536cb9229404d6d397f7be8caa3e3
77f315 <77f315@gmail.com>
2025-03-31 13:07:14 +0300

    make button modular

---
+++ naidisprojektmobile/naidisprojektmobile/Controls/CustomButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace naidisprojektmobile.Controls
+{
+    public class CustomButton : Button
+    {
+        public CustomButton()
+        {
+            BackgroundColor = Color.FromHex("#4F63AC");
+            TextColor = Color.White;
+            FontSize = 16;
+            FontAttributes = FontAttributes.Bold;
+            CornerRadius = 5;
+            HeightRequest = 75;
+            WidthRequest = 250;
+        }
+    }
+ }
